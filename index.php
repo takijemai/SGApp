91721f6477b7ec186f8c3be294eff591b6bb1a5b
takijemai <73497985+takijemai@users.noreply.github.com>
2022-02-02 19:46:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
+        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "sg");
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
